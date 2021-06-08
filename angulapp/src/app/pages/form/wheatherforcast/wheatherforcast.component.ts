77d7f72eecebee5d91fe9345194277d08be3a4ab
Satish Chorghe <chorghesatish8@outlook.com>
2021-06-08 22:05:46 +0530

    used spinner in project

--- angulapp/src/app/pages/form/wheatherforcast/wheatherforcast.component.ts
+++ angulapp/src/app/pages/form/wheatherforcast/wheatherforcast.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
+import { SelectMultipleControlValueAccessor } from "@angular/forms";
 import bsCustomFileInput from "bs-custom-file-input";
+import { delay } from "rxjs/operators";
 
 import { PagesService } from "../../../services/pages.service";
 
@@ -12,6 +14,7 @@ export class WheatherForcastComponent implements OnInit {
   // bread crumb items
   breadCrumbItems: Array<{}>;
   persons: any;
+  loadComplete: boolean = false;
 
   constructor(private pageservice: PagesService) { }
 
@@ -22,8 +25,12 @@ export class WheatherForcastComponent implements OnInit {
   }
 
   async GetWheatherList() {
+    this.loadComplete = true;
+    await new Promise(resolve => setTimeout(resolve, 5000));
+
     await this.pageservice.getPersons().
       then(aa => { this.persons = aa.Table });
     console.log(this.persons);
+    this.loadComplete = false;
   }
 }
